9974ef08d213217f37e76a2c7fe4da5fae18033d
leinad-sch <schleinadsch@gmail.com>
2025-04-14 14:46:02 +0200

    feat: add support for mixed instances policy in ASG configuration
    Introduced a new `mixed_instances_policy` variable to enable mixed instance types for Autoscaling Groups. Updated ASG resource to dynamically configure mixed instances policy, including instances distribution and overrides, when the variable is provided.

--- asg.tf
+++ asg.tf
@@ -1,3 +1,12 @@
+locals {
+  mixed_instances_policy = (
+    var.mixed_instances_policy == null ? null : {
+    instances_distribution = var.mixed_instances_policy.instances_distribution
+    launch_template        = aws_launch_template.main
+    override               = var.mixed_instances_policy.override
+  })
+}
+
 resource "aws_autoscaling_group" "main" {
   count = var.ha_mode ? 1 : 0
 
@@ -9,6 +18,46 @@ resource "aws_autoscaling_group" "main" {
   vpc_zone_identifier = [var.subnet_id]
 
   termination_policies = var.termination_policies
+
+  dynamic "mixed_instances_policy" {
+    for_each = (local.mixed_instances_policy != null ?
+      [local.mixed_instances_policy] : [])
+    content {
+      dynamic "instances_distribution" {
+        for_each = (
+          mixed_instances_policy.value.instances_distribution != null ?
+          [mixed_instances_policy.value.instances_distribution] : [])
+        content {
+          on_demand_allocation_strategy = lookup(
+            instances_distribution.value, "on_demand_allocation_strategy", null)
+          on_demand_base_capacity = lookup(
+            instances_distribution.value, "on_demand_base_capacity", null)
+          on_demand_percentage_above_base_capacity = lookup(
+            instances_distribution.value, "on_demand_percentage_above_base_capacity", null)
+          spot_allocation_strategy = lookup(
+            instances_distribution.value, "spot_allocation_strategy", null)
+          spot_instance_pools = lookup(
+            instances_distribution.value, "spot_instance_pools", null)
+          spot_max_price = lookup(
+            instances_distribution.value, "spot_max_price", null)
+        }
+      }
+      launch_template {
+        launch_template_specification {
+          launch_template_id = mixed_instances_policy.value.launch_template.id
+          version            = mixed_instances_policy.value.launch_template.version
+        }
+        dynamic "override" {
+          for_each = (mixed_instances_policy.value.override != null ?
+            mixed_instances_policy.value.override : [])
+          content {
+            instance_type = lookup(override.value, "instance_type", null)
+            weighted_capacity = lookup(override.value, "weighted_capacity", null)
+          }
+        }
+      }
+    }
+  }
 
   launch_template {
     id      = aws_launch_template.main.id
